c63f5e7074912d00b5f2307d90720ab6f2ccb17f
andyvega <andy.vega@adventistas.org>
2026-02-06 11:04:48 -0500

    Fix: Change root route to JSON response to avoid 500 error

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
+}); */
+
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// Cambiamos la vista por una respuesta JSON directa
+Route::get('/', function () {
+    return response()->json([
+        'app' => 'Campanha-K API',
+        'status' => 'Online',
+        'environment' => app()->environment(),
+        'database_connection' => 'Connected to Neon'
+    ]);
 });
